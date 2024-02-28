ffece2561cfd048fcb9dfccce6e6757aedc997dd
rcq <1551498954@qq.com>
2024-02-28 14:45:09 +0800

    add tests

--- test/chapter1.cpp
+++ test/chapter1.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 #include <glog/logging.h>
 
-TEST(chpter1, test1) {
-    
+TEST(chapter1, base_const) {
 
 }
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <glog/logging.h>
 
+
 int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
     google::InitGoogleLogging("Rcq");
